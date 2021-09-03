78780a02ec06976c970cffe6a564d0efd4e7bd73
jhansiaderu <aderujhansi1544@gmail.com>
2021-09-03 11:37:40 +0530

    video-20

--- src/App.js
+++ src/App.js
@@ -30,12 +30,25 @@ function App() {
       }, 4000);
   }
 
-  const toggleMode = ()=>{
+  const removeBodyClasses = ()=>{
+    
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-warning')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-danger')
+    //document.body.classList.remove('bg-primary');
+  }
+
+
+  const toggleMode = (cls)=>{
+    console.log(cls);
+    document.body.classList.add('bg-'+cls)
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-      document.title = "TextUtils-Dark Mode";
+      //document.title = "TextUtils-Dark Mode";
       //setInterval(() => {
        // document.title = "TextUtils is amazing Mode";
       //} , 2000);
@@ -48,7 +61,7 @@ function App() {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-      document.title = "TextUtils-Light Mode";
+     // document.title = "TextUtils-Light Mode";
     }
   }
   return (
